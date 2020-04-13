0ec49f80b2df08940c5079d78fc54c6a6ffe7951
Chet Hendrickson <Chet@hendricksonxp.com>
2020-04-13 16:06:59 -0400

    made a test

--- texttest_fixture.rb
+++ texttest_fixture.rb
@@ -1,6 +1,31 @@
 #!/usr/bin/ruby -w
 
 require File.join(File.dirname(__FILE__), 'gilded_rose')
+gold = "-------- day 0 --------
+name, sellIn, quality
++5 Dexterity Vest, 10, 20
+Aged Brie, 2, 0
+Elixir of the Mongoose, 5, 7
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 15, 20
+Backstage passes to a TAFKAL80ETC concert, 10, 49
+Backstage passes to a TAFKAL80ETC concert, 5, 49
+Conjured Mana Cake, 3, 6
+
+-------- day 1 --------
+name, sellIn, quality
++5 Dexterity Vest, 9, 19
+Aged Brie, 1, 1
+Elixir of the Mongoose, 4, 6
+Sulfuras, Hand of Ragnaros, 0, 80
+Sulfuras, Hand of Ragnaros, -1, 80
+Backstage passes to a TAFKAL80ETC concert, 14, 21
+Backstage passes to a TAFKAL80ETC concert, 9, 50
+Backstage passes to a TAFKAL80ETC concert, 4, 50
+Conjured Mana Cake, 2, 5
+
+"
 
 puts "OMGHAI!"
 items = [
@@ -21,13 +46,26 @@ if ARGV.size > 0
   days = ARGV[0].to_i + 1
 end
 
+
+
 gilded_rose = GildedRose.new items
+_lead =""
 (0...days).each do |day|
-  puts "-------- day #{day} --------"
-  puts "name, sellIn, quality"
+  _lead +=  "-------- day #{day} --------\n"
+  _lead += "name, sellIn, quality\n"
   items.each do |item|
-    puts item
+    _lead += item.to_s + "\n"
   end
-  puts ""
+  _lead += "\n"
   gilded_rose.update_quality
 end
+
+gold2 = gold.gsub("\r\n/m", "")
+
+puts gold2 == _lead
+# puts "gold2"
+# puts gold2
+
+# puts "Lead"
+# puts _lead
+
